103cff41a43558ba473fd93ac743486247ef0ffb
dieguischa <dfchacona@unal.edu.co>
2021-09-23 13:54:56 -0500

    Image

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+const IMAGE_ROUTE = "./assets/friends.jpg"
+
 var app = new Vue (
     {
         el: '#app',
         data: {
             product: 'Socks',
-            color: 'White'
+            color: 'White',
+            image: IMAGE_ROUTE
         }
     }
 );
