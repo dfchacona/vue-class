75c1a0091b72a9fd35d4916cff0ef73f9449cfb9
AndresBarr <andres@clickaway.io>
2021-09-24 13:07:17 -0500

    Class 7

--- main.js
+++ main.js
@@ -2,24 +2,26 @@ var app = new Vue (
     {
         el: '#app',
         data: {
+            brand: 'Vue Mastery',
             product: 'Socks',
             color: 'White',
-            image: "./assets/friends.jpg",
+            selectedVariant: 0,
             link: '#hola',
-            inStock: false,
             inventory: 10,
             onSale: true,
             details: ["80% cotton", "20% polyester", "Unisex"],
             variants: [
                 {
-                    variantId: "2234", 
-                    variantColor: "green",
-                    variantImage:"./assets/friends.jpg"
+                    variantId: 2234, 
+                    variantColor: 'green',
+                    variantImage:'./assets/friends.jpg',
+                    variantQuantity: 10
                 },
                 {
-                    variantId: "2235", 
-                    variantColor: "blue",
-                    variantImage: "./assets/Venom.jpeg"
+                    variantId: 2235, 
+                    variantColor: 'blue',
+                    variantImage: './assets/Venom.jpeg',
+                    variantQuantity: 0
                 }
             ],
             cart: 0            
@@ -31,9 +33,25 @@ var app = new Vue (
             removeCart: function(){
                 this.cart -=1
             },
-            updateProduct: function(variantImage){
-                this.image =variantImage
+            updateProduct: function(index){
+                this.selectedVariant =index,
+                console.log(index)
             }
+        },
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product
+            },
+            image(){
+                return this.variants[this.selectedVariant].variantImage
+            },
+            inStock(){
+                return this.variants[this.selectedVariant].variantQuantity
+            },
+            onSales (){
+                return this.brand + ' ' + this.product + ' are on Sale!'
+            }
+
         }
     }
 );
